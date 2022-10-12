ea2620e7bb11212fbdeaeb5dbb5034d84be9fcaf
Xu Han <hxhxhx88@gmail.com>
2022-10-12 16:28:30 +0200

    configured eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react"
 
-function App() {
-  return (
-    <div>Hello world</div>
-  );
+function App (): JSX.Element {
+    return <div>Hello world</div>
 }
 
-export default App;
+export default App
